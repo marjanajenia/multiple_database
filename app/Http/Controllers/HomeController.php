1e792b47cc03cbb3c1ae6a10f07257bfa9237540
marjanajenia <marjanajenia98@gmail.com>
2025-07-06 00:00:51 +0600

    Article info insert into db

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -13,5 +14,14 @@ class HomeController extends Controller
             'status' => 'active',
         ]);
         return 'Category created';
+    }
+    public function articleAdd(){
+        Article::create([
+            'name' => 'Lifeless',
+            'author' => 'Marjana',
+            'description' => 'Life is hell$%$',
+            'category_id' => 1,
+        ]);
+        return 'My Current situation';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/', function () {
 });
 
 Route::get('/add', [HomeController::class, 'categoryAdd']);
+Route::get('/art', [HomeController::class, 'articleAdd']);
